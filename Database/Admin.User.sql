2435fb1b577be01025d77ae4cefee05afff272da
Anabelt-hub <anabelt@clemson.edu>
2026-09-11 13:26:20 -0400

    Update Admin.User.sql
    password not plain text and not null

--- Database/Admin.User.sql
+++ Database/Admin.User.sql
@@ -2,6 +2,6 @@
     admin_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    email VARCHAR(100),
-    password VARCHAR(255)
+    email VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
 );
