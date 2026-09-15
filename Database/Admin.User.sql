5a227a5286992162abbae2748794cab018011000
Anabelt-hub <anabelt@clemson.edu>
2026-09-15 12:39:30 -0400

    Update Admin.User.sql
    decided to use user_id universally

--- Database/Admin.User.sql
+++ Database/Admin.User.sql
@@ -1,7 +1,11 @@
   CREATE TABLE AdminUser (
-    admin_id INT PRIMARY KEY,
+    
+    user_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(100) NOT NULL,
     password_hash VARCHAR(255) NOT NULL
+    
+    FOREIGN KEY (user_id) REFERENCES UserAccount(user_id)
+    
 );
