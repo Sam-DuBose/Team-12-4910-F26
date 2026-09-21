291d233f6fb07c9588c4e424dab2bb1404e601ed
lee-truong-0402 <lvtruon@clemson.edu>
2026-09-21 10:45:07 -0400

    Added some back end to test the front end

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.about import about_bp
+from routes.login import auth_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../frontend')
 
 app.register_blueprint(about_bp)
+app.register_blueprint(auth_bp)
+
+@app.route('/')
+def home():
+    return render_template('FrontPage.html')
 
 @app.get("/api/health")
 def health():
-  return {
-  "status": "ok"
-  }
+    return {
+        "status": "ok"
+    }
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
